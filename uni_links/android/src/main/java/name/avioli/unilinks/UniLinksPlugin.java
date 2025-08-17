a456a68b76486e581b78cdaa361691a7e29508dc
Mayur Pitroda <91420335+mayurpitroda96@users.noreply.github.com>
2025-08-17 17:59:57 +0530

    Update UniLinksPlugin.java

--- uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
+++ uni_links/android/src/main/java/name/avioli/unilinks/UniLinksPlugin.java
@@ -13,26 +13,32 @@ import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.EventChannel;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
 
+/**
+ * UniLinksPlugin (New API for Flutter 3.29+)
+ */
 public class UniLinksPlugin
         implements FlutterPlugin,
-                MethodChannel.MethodCallHandler,
-                EventChannel.StreamHandler,
-                ActivityAware,
-                PluginRegistry.NewIntentListener {
+                   MethodChannel.MethodCallHandler,
+                   EventChannel.StreamHandler,
+                   ActivityAware {
 
     private static final String MESSAGES_CHANNEL = "uni_links/messages";
     private static final String EVENTS_CHANNEL = "uni_links/events";
 
     private BroadcastReceiver changeReceiver;
-
     private String initialLink;
     private String latestLink;
     private Context context;
     private boolean initialIntent = true;
 
+    private MethodChannel methodChannel;
+    private EventChannel eventChannel;
+
+    // Handle incoming intents
     private void handleIntent(Context context, Intent intent) {
+        if (intent == null) return;
+
         String action = intent.getAction();
         String dataString = intent.getDataString();
 
@@ -42,7 +48,9 @@ public class UniLinksPlugin
                 initialIntent = false;
             }
             latestLink = dataString;
-            if (changeReceiver != null) changeReceiver.onReceive(context, intent);
+            if (changeReceiver != null) {
+                changeReceiver.onReceive(context, intent);
+            }
         }
     }
 
@@ -51,12 +59,7 @@ public class UniLinksPlugin
         return new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                // NOTE: assuming intent.getAction() is Intent.ACTION_VIEW
-
-                // Log.v("uni_links", String.format("received action: %s", intent.getAction()));
-
                 String dataString = intent.getDataString();
-
                 if (dataString == null) {
                     events.error("UNAVAILABLE", "Link unavailable", null);
                 } else {
@@ -66,79 +69,79 @@ public class UniLinksPlugin
         };
     }
 
+    // -------------------------
+    // FlutterPlugin lifecycle
+    // -------------------------
+
     @Override
-    public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-        this.context = flutterPluginBinding.getApplicationContext();
-        register(flutterPluginBinding.getBinaryMessenger(), this);
-    }
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        this.context = binding.getApplicationContext();
+        BinaryMessenger messenger = binding.getBinaryMessenger();
 
-    private static void register(BinaryMessenger messenger, UniLinksPlugin plugin) {
-        final MethodChannel methodChannel = new MethodChannel(messenger, MESSAGES_CHANNEL);
-        methodChannel.setMethodCallHandler(plugin);
+        methodChannel = new MethodChannel(messenger, MESSAGES_CHANNEL);
+        methodChannel.setMethodCallHandler(this);
 
-        final EventChannel eventChannel = new EventChannel(messenger, EVENTS_CHANNEL);
-        eventChannel.setStreamHandler(plugin);
-    }
-
-    /** Plugin registration. */
-    public static void registerWith(@NonNull PluginRegistry.Registrar registrar) {
-        // Detect if we've been launched in background
-        if (registrar.activity() == null) {
-            return;
-        }
-
-        final UniLinksPlugin instance = new UniLinksPlugin();
-        instance.context = registrar.context();
-        register(registrar.messenger(), instance);
-
-        instance.handleIntent(registrar.context(), registrar.activity().getIntent());
-        registrar.addNewIntentListener(instance);
+        eventChannel = new EventChannel(messenger, EVENTS_CHANNEL);
+        eventChannel.setStreamHandler(this);
     }
 
     @Override
-    public void onDetachedFromEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {}
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        if (methodChannel != null) methodChannel.setMethodCallHandler(null);
+        if (eventChannel != null) eventChannel.setStreamHandler(null);
+        methodChannel = null;
+        eventChannel = null;
+    }
 
+    // -------------------------
+    // EventChannel handlers
+    // -------------------------
     @Override
-    public void onListen(Object o, EventChannel.EventSink eventSink) {
+    public void onListen(Object arguments, EventChannel.EventSink eventSink) {
         changeReceiver = createChangeReceiver(eventSink);
     }
 
     @Override
-    public void onCancel(Object o) {
+    public void onCancel(Object arguments) {
         changeReceiver = null;
     }
 
+    // -------------------------
+    // MethodChannel handler
+    // -------------------------
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
-        if (call.method.equals("getInitialLink")) {
-            result.success(initialLink);
-        } else if (call.method.equals("getLatestLink")) {
-            result.success(latestLink);
-        } else {
-            result.notImplemented();
+        switch (call.method) {
+            case "getInitialLink":
+                result.success(initialLink);
+                break;
+            case "getLatestLink":
+                result.success(latestLink);
+                break;
+            default:
+                result.notImplemented();
+                break;
         }
     }
 
+    // -------------------------
+    // ActivityAware (for intents)
+    // -------------------------
     @Override
-    public boolean onNewIntent(Intent intent) {
-        this.handleIntent(context, intent);
-        return false;
-    }
-
-    @Override
-    public void onAttachedToActivity(@NonNull ActivityPluginBinding activityPluginBinding) {
-        activityPluginBinding.addOnNewIntentListener(this);
-        this.handleIntent(this.context, activityPluginBinding.getActivity().getIntent());
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        binding.addOnNewIntentListener(intent -> {
+            handleIntent(context, intent);
+            return false;
+        });
+        handleIntent(context, binding.getActivity().getIntent());
     }
 
     @Override
     public void onDetachedFromActivityForConfigChanges() {}
 
     @Override
-    public void onReattachedToActivityForConfigChanges(
-            @NonNull ActivityPluginBinding activityPluginBinding) {
-        activityPluginBinding.addOnNewIntentListener(this);
-        this.handleIntent(this.context, activityPluginBinding.getActivity().getIntent());
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+        onAttachedToActivity(binding);
     }
 
     @Override
